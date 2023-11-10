285b4d1d3aadaad453e621fcf4a8295dd1ebbb7b
邓 <359230294@qq.com>
2023-11-10 11:03:48 +0800

    init project

---
+++ src/main/java/com/ot/onetool/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ot.onetool.Service.Impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ot.onetool.Service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+}
---
+++ src/main/java/com/ot/onetool/Service/UserService.java
@@ -0,0 +1,8 @@
+package com.ot.onetool.Service;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+
+public interface UserService {
+}
