0a510fcf97ba8e171a1400b44c5954988b9e2314
denglianrui <359230294@qq.com>
2023-11-15 10:28:31 +0800

    新增注册接口

--- src/main/java/com/ot/onetool/Controller/UserController.java
+++ src/main/java/com/ot/onetool/Controller/UserController.java
@@ -19,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>
  *  前端控制器
@@ -30,25 +33,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @Api("用户接口管理")
+
 @RequiredArgsConstructor
 public class UserController {
     private final UserMapper userMapper;
     private final IUserService userService;
 
-    @PostMapping
+    @PostMapping("/register")
     @ApiOperation("新增用户,实现用户注册功能")
-    public Result insertUser(UserPO userPO){
-        Result result=new Result();
+    public Result<Map> insertUser(UserPO userPO){
+        Result<Map> result=new Result();
         boolean save = userService.save(userPO);
         if (save){
             result.setState(1);
             result.setMsg("保存成功");
-//
-//            String sign = TokenUtil.sign(userPO);
-//            Gson gson=new Gson();
-//            JsonObject jsonObject=new JsonObject();
-//            jsonObject.add("userVO", gson.toJsonTree(BeanUtil.toBean(userPO, UserVO.class)));
-//            jsonObject.add("token", gson.toJsonTree(sign));
+
+            String sign = TokenUtil.sign(userPO);
+            Gson gson=new Gson();
+            Map<Object,Object> data=new HashMap<>();
+            data.put("userVO",BeanUtil.toBean(userPO, UserVO.class));
+            data.put("token",sign);
+            result.setData(data);
 
         }else {
             result.setState(0);
